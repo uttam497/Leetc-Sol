5f8db7fbf9ae69653427ceb3b107f43b81d57d3c
uttam497 <81505448+uttam497@users.noreply.github.com>
2022-11-11 23:59:59 +0530

    Time: 43 ms (72.52%), Space: 79.4 MB (59.07%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,47 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if (k == 0) return false;
-
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int integer = nums[i];
-            if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
-                return true;
-            hashMap.put(integer, i);
+        int i=0;
+        int j=0;
+        int size=nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        while(j<size)
+        {
+            if(i==j)    map.put(nums[j],j);
+            
+            if(j!=i && j-i<k)
+            {
+                if(map.containsKey(nums[j]))
+                   {
+                       return true;
+                   }
+                  map.put(nums[j],j);
+            }
+                   else  if(j-i==k)
+                   {
+                       if(map.containsKey(nums[j]))
+                       {
+                           return true;
+                       }
+                       map.put(nums[j],j);
+                       map.remove(nums[i]);
+                       i++;
+                       
+                   }
+            j++;
         }
         return false;
+
+       
     }
 }
+
+
+ // Map<Integer, Integer> hashMap = new HashMap<>();
+ //        for (int i = 0; i < nums.length; i++) {
+ //            int integer = nums[i];
+ //            if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
+ //                return true;
+ //            hashMap.put(integer, i);
+ //        }
+ //        return false;

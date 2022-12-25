e22fcd577b5e43744aa384877355794f270f694b
uttam497 <81505448+uttam497@users.noreply.github.com>
2022-12-25 22:34:17 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (72.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,34 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        int q2=0;
-     int q1=0;
-        int memo[]=new int[46];
-return climb(q1,q2,n,memo);
-    }
-    public int climb(int q1,int q2,int n,int memo[])
-    {
-        if(n==0)
-        {
-            memo[0]=0;
-            return 0;
-        }
-         if(n == 1){
-             memo[1]=1;
-        return 1;
-    }
-    if(n == 2){
-        memo[2]=2;
-        return 2;
-    }
-        if(memo[n]!=0)
-        {
-            return memo[n];
-        }
         
-       
-    q1=climb(q1,q2,n-1,memo);
-    q2=climb(q1,q2,n-2,memo);
-        memo[n]=q1+q2;
-        return memo[n];
+        int memo[]=new int[n+1];
+        
+return climb(n,memo);
+    }
+    public int climb(int n,int dp[])
+    {
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=0)    return dp[n];
+int ans=climb(n-1,dp)+climb(n-2,dp);
+        dp[n]=ans;
+        return dp[n];
     }
 }

66f8f87e7aee0d47ca2d08d8f13e5f1791fec48c
uttam497 <81505448+uttam497@users.noreply.github.com>
2023-03-07 21:59:45 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (39.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
         
-        int memo[]=new int[n+1];
-        
-return climb(n,memo);
+        int dp[]=new int[n+1];
+        return climb(n,dp);
     }
     public int climb(int n,int dp[])
     {
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=0)    return dp[n];
-int ans=climb(n-1,dp)+climb(n-2,dp);
-        dp[n]=ans;
-        return dp[n];
+    if(n<0) return 0;
+        if(n==0)    return 1;
+        if(dp[n]!=0)   return dp[n];
+        
+        
+        dp[n]= climb(n-1,dp)+climb(n-2,dp);
+    return dp[n];
     }
 }
+
+
+
